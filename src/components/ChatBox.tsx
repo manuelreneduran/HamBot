a6872ce2d4191377dc7294f4af356af76f664719
Manuel Duran <manuelduran@Manuels-MacBook-Pro.local>
2024-05-21 11:39:07 -0700

    more styling to message chatbox body

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -3,12 +3,24 @@ import hamiltonAvatar from "../assets/hamilton_avatar.jpeg";
 import { colors } from "../styles/colors";
 import { exampleMessages } from "../utils/examples";
 import MessageList from "./MessageList";
+import { useEffect, useRef } from "react";
 
-const ChatBox = () => {
+type ChatBoxProps = {
+  children: React.ReactNode;
+};
+const ChatBox = ({ children }: ChatBoxProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  }, [children]); // Dependency on children to re-scroll when new content is added
+
   return (
     <Stack
       sx={{
         border: `1px solid ${colors.border.primary}`,
+        height: "100%",
       }}
       className="chatbox-container"
     >
@@ -56,7 +68,14 @@ const ChatBox = () => {
           </Stack>
         </Stack>
       </Stack>
-      <Stack flex={1} sx={{ padding: ".5rem" }} className="chatbox-body">
+      <Stack
+        ref={containerRef}
+        sx={{
+          overflowY: "auto",
+          padding: ".5rem",
+        }}
+        className="chatbox-body"
+      >
         <MessageList messages={exampleMessages} />
       </Stack>
       <Stack
